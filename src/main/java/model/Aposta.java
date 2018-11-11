b532406e13c77b90b46fc0ae0855c5ec657cb631
RafaelSouzaValle <rafaelsouzavalle@gmail.com>
2018-11-10 23:26:49 -0200

    Crie um método que compara os resultados de dois campeonatos
    Na classe de aposta, criação de um método que recebe dois campeonatos como argumento (um de aposta e outro do campeonato em si). Após comparar os dois, retorna a quantidade de resultados iguais.

--- src/main/java/model/Aposta.java
+++ src/main/java/model/Aposta.java
@@ -1,27 +1,72 @@
 package model;
 
+import java.util.List;
+
 public class Aposta {
 
 	// Recebe a pontuação total obtida com a aposta
-	private int pontuacao;
-	
+	private static int pontuacao;
+
 	// Recebe a instância de uma campeonato
 	private Campeonato campeonatoApostado;
-	
-	//Cria um campeonato sorteando jogos e resultados para cada rodada
+
+	// Cria um campeonato sorteando jogos e resultados para cada rodada
 	public Campeonato criaCampeonato() {
-		
+
 		campeonatoApostado = new Campeonato();
-		
+
 		campeonatoApostado.executaCampeonato();
-		
+
 		return campeonatoApostado;
 	}
 
-	//Compara campeonato do Banco de dados com o da aposta
-	//e calcula a pontuação obtida
-	public void calculaPontuacao() {
-		//TODO -- Aqui entrará o método
+	// Compara campeonato do Banco de dados com o da aposta
+	// e calcula a pontuação obtida
+	public static int calculaPontuacao(Campeonato campeonato, Campeonato aposta) {
+
+		// Recebe a lista de rodadas do campeonato passado como argumento
+		List<Rodada> rodadas = campeonato.getRodadas();
+
+		int numRodada = 0;
+		// Lê as rodadas do campeonato
+		for (Rodada rodada : rodadas) {
+
+			// Recebe a lista de partidas da rodada atual
+			List<Partida> partidas = rodada.getPartidas();
+
+			int numPartida = 0;
+			// Lê as partidas da rodada atual
+			for (Partida partida : partidas) {
+
+				// Recebe resulrado das partidas atuais
+				String resultado = partida.getResultado();
+
+				String resultadoApostado = aposta.getRodadas().get(numRodada).getPartidas().get(numPartida)
+						.getResultado();
+
+				if (resultado.equals(resultadoApostado)) {
+					pontuacao++;
+				}
+				numPartida++;
+			}
+			numRodada++;
+		}
+		return pontuacao;
 	}
-	
+
+	public int getPontuacao() {
+		return pontuacao;
+	}
+
+	public void setPontuacao(int pontuacao) {
+		this.pontuacao = pontuacao;
+	}
+
+	public Campeonato getCampeonatoApostado() {
+		return campeonatoApostado;
+	}
+
+	public void setCampeonatoApostado(Campeonato campeonatoApostado) {
+		this.campeonatoApostado = campeonatoApostado;
+	}
 }
