52b42b1823a0a6a7ddb24b4cb1c1c879395a3bcf
RafaelSouzaValle <rafaelsouzavalle@gmail.com>
2018-11-11 01:45:01 -0200

    Crie métodos para salvar e carregar usuário
    Crie métodos para salvar e carregar usuário (gravar/recuperar dados do Banco de dados)

--- src/main/java/jedis/JedisManager.java
+++ src/main/java/jedis/JedisManager.java
@@ -6,6 +6,7 @@ import java.util.List;
 import model.Campeonato;
 import model.Partida;
 import model.Rodada;
+import model.Usuario;
 import redis.clients.jedis.Jedis;
 
 public class JedisManager {
@@ -13,7 +14,7 @@ public class JedisManager {
 	// Conecta no Redis do localhost
 	public static Jedis jedis = new Jedis("localhost");
 
-	//Registra dado no servidor Redis
+	// Registra dado no servidor Redis
 	public static void gravarString(String chave, String valor) {
 		jedis.set(chave, valor);
 	}
@@ -52,50 +53,50 @@ public class JedisManager {
 		}
 	}
 
-	// Carrega os dados armazenados no Banco de Dados e 
+	// Carrega os dados armazenados no Banco de Dados e
 	// retorna uma instaância de Campeonato com contendo as informações
 	public static Campeonato carregaCampeonatoSalvo() {
-		//Cria uma instância de Campeonato para receber
-		//as informações extraídas do BD
+		// Cria uma instância de Campeonato para receber
+		// as informações extraídas do BD
 		Campeonato campeonato = new Campeonato();
-		
+
 		// Lê as rodadas do campeonato
 		for (int i = 0; i < Campeonato.getQuantidadeRodadas(); i++) {
 
 			Rodada rodada = new Rodada();
-			
+
 			// Lê as partidas da rodada atual
 			for (int j = 0; j < Rodada.getQuantidadePartidas(); j++) {
 				String chave = i + "" + j;
 
-				//Lista que recebe os Strings lidos do banco de dados
+				// Lista que recebe os Strings lidos do banco de dados
 				List<String> listaDados = new ArrayList<String>();
 
 				listaDados.add(jedis.lrange(chave, 0, 2).toString());
-				
+
 				// Separa cada String retornado pelo
 				// servidor, o separa em 3 partes e
 				// atribui cada um atributo de uma nova partida
 				for (String stringPartida : listaDados) {
-					
+
 					// Array de Strings que recebe em cada índice
 					// uma parte de um String retornado
 					String[] arrayPartida = stringPartida.split(", ");
-					
+
 					// Atribui cada parte do Array a um String
 					String time1 = arrayPartida[0].substring(1);
 					String time2 = arrayPartida[1];
 					String resultado = arrayPartida[2].substring(0, arrayPartida[2].length() - 1);
-					
+
 					// Atribui os Strings aos atributos de cada partida
 					Partida partida = new Partida();
 					partida.setNomeTime1(time1);
 					partida.setNomeTime2(time2);
 					partida.setResultado(resultado);
-					
+
 					// Adiciona a partida na rodada
 					rodada.getPartidas().add(partida);
-					
+
 					// Imprime uma partida (apenas para testes)
 //					System.out.println(
 //							"Rodada " + (i + 1) + " | Partida " + (j + 1)
@@ -105,13 +106,38 @@ public class JedisManager {
 				}
 
 			}
-			
+
 			// Adiciona uma rodada no campeonato
 			campeonato.getRodadas().add(rodada);
 		}
-		
+
 		// Retorna o campeonato
 		return campeonato;
 	}
-	
+
+	public static void salvaUsuario(Usuario usuario) {
+
+		String apelido = usuario.getApelido();
+
+		jedis.rpush(apelido, usuario.getApelido());
+		jedis.rpush(apelido, usuario.getNome());
+		jedis.rpush(apelido, usuario.getNascimento().toString());
+		jedis.rpush(apelido, usuario.getGenero().toString());
+		jedis.rpush(apelido, usuario.getEndereco().toString());
+		jedis.rpush(apelido, usuario.getPontuacao().toString());
+
+	}
+
+	public static Usuario carregaUsuarioSalvo(String apelido) {
+
+		List<String> dadosUsuario = jedis.lrange(apelido, 0, 5);
+		
+		for (String string : dadosUsuario) {
+			System.out.println();
+		}
+		
+		return null;
+
+	}
+
 }
